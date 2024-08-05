a98ad9cdc5644b9afe62e324744205ccbd4e74bc
Achilles06 <92751219+Achilles06@users.noreply.github.com>
2024-08-05 13:18:54 -0400

    commit

---
+++ basics_and_syntax.js
@@ -0,0 +1,17 @@
+//Task 1
+let studentName;
+let studentAge;
+let sutdentGrade;
+let isPresent;
+
+//Task 2
+studentName = "Emily"
+studentAge = 18;
+sutdentGrade = 12;
+isPresent = true;
+
+//Task 3
+console.log("Student Name:" + studentName);
+console.log("Student Age: " + studentAge);
+console.log("Student Grade: " + sutdentGrade);
+console.log("Attendance Status: " + (isPresent ? "Present" : "Absent"))
